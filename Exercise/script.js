f60dbdf7d61078c8a4f1fda6e0ab2ec4502a3322
Chimoha31 <mokochii1108@gmail.com>
2022-01-25 09:35:32 -0800

    Reviewed and Done

--- Exercise/script.js
+++ Exercise/script.js
@@ -5,38 +5,36 @@ Exercise 01
 -----------
 Highlight all of the words over 8 characters long in the paragraph text (with a yellow background for example)
 ----------------------------------------------------------------*/
-let p = document.querySelector('p');
-let words01 = p.innerHTML.split(' ');
-console.log(words01);
+let p = document.querySelector("p");
+p.innerHTML = p.innerHTML
+  .split(" ")
+  .map((word) => {
+    let span = document.createElement("span");
+    // span.setAttribute("style", "background-color: pink");
+    if (word.length > 8) {
+      span.innerHTML = word;
+      console.log(span);
+      console.log(word);
 
-
-words01.map((word) => {
-  let span = document.createElement("span");
-  span.setAttribute("style", "color: pink");
-  if(word.length > 8) {
-    span.innerHTML = word;
-    console.log(span);
-    console.log(word);
-
-    // ↓ここをどうにかする
-    p.innerHTML.replace(word, span);
-  }
-});
-
+      // ↓ここをどうにかする
+      // p.innerHTML.replace(word, span);
+      return `<span style="background-color:pink">${word}</span>`;
+    } else {
+      return word;
+    }
+  })
+  .join(" ");
 /*------------------------------------------------------------------
 Exercise 02
 -----------
 Add a link back to the source of the text after the paragraph tag.
 (http://officeipsum.com/)
 -------------------------------------------------------------------*/
-const p02 = document.querySelector('p');
 const a = document.createElement("a");
 a.setAttribute("href", "https://www.yahoo.co.jp");
 console.log(a);
-a.innerText = 'https://www.yahoo.co.jp';
-p02.after(a);
-
-
+a.innerText = "https://www.yahoo.co.jp";
+p.after(a);
 
 /*------------------------------------------------------------------
   Exercise 03
@@ -44,9 +42,7 @@ p02.after(a);
   Split each new sentence on to a separate line in the paragraph text.
   A sentence can be assumed to be a string of text terminated with a period (.)
 --------------------------------------------------------------------*/
-p.innerHTML = p.innerHTML.replaceAll('.', '<br>');
-
-
+p.innerHTML = p.innerHTML.replaceAll(".", "<br>");
 
 /* ------------------------------------------------------------------
   Exercise 04
@@ -54,13 +50,9 @@ p.innerHTML = p.innerHTML.replaceAll('.', '<br>');
   Count the number of words in the paragraph tag and display the count afer the heading.
   You can assume that all words are separated by one singular whitespace.
 ---------------------------------------------------------------------*/
-const h1 = document.querySelector('h1');
-const words = document.querySelector("p");
-const eachWord01 = words.innerHTML.split(' ');
+const eachWord01 = p.textContent.trim().split(" ");
 console.log(eachWord01.length);
-h1.textContent = eachWord01.length;
-
-
+p.before(eachWord01.length);
 
 
 /*---------------------------------------------------------------
@@ -68,10 +60,5 @@ h1.textContent = eachWord01.length;
   -----------
   Replace all question marks (?) with thinking faces (🤔) and exclamation marks (!) with astonished faces (😲) 
 -----------------------------------------------------------------*/
-const eachWord02 = document.querySelector("p");
-eachWord02.innerHTML = eachWord02.innerHTML.replaceAll("?", "🤔")
-eachWord02.innerHTML = eachWord02.innerHTML.replaceAll("!", "😲");
-
-
-
-
+p.innerHTML = p.innerHTML.replaceAll("?", "🤔").replaceAll("!", "😲");
+// p.innerHTML = p.innerHTML.replaceAll("!", "😲");
